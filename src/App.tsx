d355684491f5819a41e39f3cdc9ac855fa548d36
Max P <maxfarseer@gmail.com>
2019-03-07 09:02:41 +0300

    add reach router with typings & url params

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
-import { Intro } from './Intro'
+
+import { Link, Router } from '@reach/router'
+
+import { About } from './pages/About'
+import { Home } from './pages/Home'
 
 import './App.css'
 
-const App = () => {
+const App = ({ children }: any) => {
   return (
     <div className="container">
-      <p>Стартер для нового проекта.</p>
-      <Intro name="Max" age={30} />
+      <h1>TS starter with stuff</h1>
+      <nav>
+        <Link to="/">Home</Link> <Link to="about/me">About me</Link>{' '}
+        <Link to="about/public">About maxpfrontend</Link>
+      </nav>
+
+      {children}
     </div>
   )
 }
 
-export { App }
+const RoutedApp = () => {
+  return (
+    <Router>
+      <App path="/">
+        <Home path="/" />
+        <About path="/about/:source" />
+      </App>
+    </Router>
+  )
+}
+
+export { RoutedApp }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+import { RouteComponentProps } from '@reach/router'
+
+const Home: React.FC<RouteComponentProps> = () => {
+  return <p>Hello</p>
+}
+
+export { Home }
